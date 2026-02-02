8dfb2ea1a7ab33f06884909c9628ab9820545d7b
Seenu <55078143+Seenu-2002@users.noreply.github.com>
2026-02-02 15:52:29 +0530

    - Added theme variables and drawable resources

---
+++ core/design_system/src/main/java/com/seenu/dev/android/smartstep/design_system/theme/SmartStepTheme.kt
@@ -0,0 +1,96 @@
+package com.seenu.dev.android.smartstep.design_system.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.seenu.dev.android.core.design_system.R
+
+val Inter = FontFamily(
+    Font(R.font.inter_regular, weight = FontWeight.Normal),
+    Font(R.font.inter_medium, weight = FontWeight.Medium),
+    Font(R.font.inter_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.inter_bold, weight = FontWeight.Bold),
+)
+
+val Typography.bodyLargeMedium: TextStyle
+    get() = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    )
+
+val Typography.bodyMediumMedium: TextStyle
+    get() = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    )
+
+val ColorScheme.buttonSecondary: Color
+    get() = ButtonSecondary
+
+val ColorScheme.textWhite: Color
+    get() = TextWhite
+
+val ColorScheme.backgroundSecondary: Color
+    get() = BackgroundSecondary
+
+val ColorScheme.backgroundTertiary: Color
+    get() = BackgroundTertiary
+
+val ColorScheme.backgroundWhite: Color
+    get() = BackgroundWhite
+
+val ColorScheme.backgroundWhite20: Color
+    get() = BackgroundWhite20
+
+@Composable
+fun SmartStepTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = lightColorScheme(
+            primary = ButtonPrimary,
+            onPrimary = TextPrimary,
+            onSecondary = TextSecondary,
+            background = BackgroundMain,
+            surface = BackgroundMain,
+            outline = StrokeMain
+        ),
+        typography = MaterialTheme.typography.copy(
+            titleMedium = TextStyle(
+                fontSize = 18.sp,
+                lineHeight = 24.sp,
+                fontFamily = Inter,
+                fontWeight = FontWeight.Medium
+            ),
+            bodyLarge = TextStyle(
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                fontFamily = Inter,
+                fontWeight = FontWeight.Normal
+            ),
+            bodyMedium = TextStyle(
+                fontSize = 14.sp,
+                lineHeight = 18.sp,
+                fontFamily = Inter,
+                fontWeight = FontWeight.Normal
+            ),
+            bodySmall = TextStyle(
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+                fontFamily = Inter,
+                fontWeight = FontWeight.Normal
+            )
+        ),
+        content = content
+    )
+}
