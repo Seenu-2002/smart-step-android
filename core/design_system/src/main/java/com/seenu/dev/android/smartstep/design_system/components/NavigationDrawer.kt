dcef3e3755d3d7d7eec384274304bd233c18dec1
PARJAY <parjaylearnit@gmail.com>
2026-02-23 11:53:28 +0800

    Moving the NavigationDrawer.kt from app.main to core.design_system.main/component

--- core/design_system/src/main/java/com/seenu/dev/android/smartstep/design_system/components/NavigationDrawer.kt
+++ core/design_system/src/main/java/com/seenu/dev/android/smartstep/design_system/components/NavigationDrawer.kt
@@ -1,4 +1,4 @@
-package com.seenu.dev.android.smartstep.navigation
+package com.seenu.dev.android.smartstep.design_system.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
